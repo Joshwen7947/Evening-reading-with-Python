aea975215a5c3c24de4cb2588c0dadb375e7ba04
Josh Wenner <68701564+Joshwen7947@users.noreply.github.com>
2021-03-27 22:55:25 +0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,18 @@
+# print('Hello World!')
+# print('Starting to learn Python')
+message = "Hello world message"
+print(message)
+
+message = "will I learn python faster than foundations?"
+print(message)
+
+name = "josh wenner"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "josh"
+last_name = "wenner"
+full_name = f"{first_name} {last_name}"
+message = f"Hello there, {full_name.title()}!!"
+print(message)
